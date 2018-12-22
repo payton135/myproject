ceeb46c246982d87b3eb7982a9dd760e4f166bab
payton135 <45234044+payton135@users.noreply.github.com>
2018-12-23 00:26:29 +0800

    Initial commit

---
+++ src/main/java/com/payton/Hello.java
@@ -0,0 +1,9 @@
+package com.payton;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("hello world");
+        Person p = new Person();
+        p.hello();
+    }
+}
