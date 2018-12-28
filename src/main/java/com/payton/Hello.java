ad47b2b0aead3aca898a6800ea3db31d6b5122a5
payton135 <45234044+payton135@users.noreply.github.com>
2018-12-28 17:58:57 +0800

    Java and Kotlin variable

--- src/main/java/com/payton/Hello.java
+++ src/main/java/com/payton/Hello.java
@@ -5,5 +5,14 @@ public class Hello {
         //System.out.println("hello world");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 20;
+
+        char c = 'a';
+        Character c2 = 'b';
+        byte b = 30;
+        float weight = 65.5f;
+        boolean enroll = true;
+        String name = "Payton";
     }
 }
--- src/main/kotlin/com/payton/Hello.kt
+++ src/main/kotlin/com/payton/Hello.kt
@@ -4,6 +4,12 @@ fun main(args: Array<String>) {
     //println("Hello kotlin")
     val h = Human();
     h.hello()
+    var age = 19;
+    age = 20;
+    var weight : Float = 66.5f;
+    val name : String;
+    name = "Payton";
+
 }
 
 class Human(){
